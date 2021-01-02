527c2414c236cae1a9017d5bcf43e2007862d120
npjs444444 <npjs444444@gmail.com>
2021-01-02 10:39:55 +0545

    all-fixed

--- app.js
+++ app.js
@@ -10,9 +10,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
-  console.log(`Server running on port http://localhost:${PORT}`);
-=======
   console.log(`Server is running on port ${PORT}`);
->>>>>>> b00d65cb1e2f9cfa134589eb4882431b3f7c8ccc
 });
