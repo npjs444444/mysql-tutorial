b00d65cb1e2f9cfa134589eb4882431b3f7c8ccc
npjs <67056966+npjs444444@users.noreply.github.com>
2021-01-02 10:32:48 +0545

    Update app.js

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3030;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
