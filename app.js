bf0d24fa34f9b90a1f3e97d65e8a37363a8fb7d2
npjs444444 <npjs444444@gmail.com>
2021-01-02 10:31:33 +0545

    express-initial-setup

---
+++ app.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const PORT = process.env.PORT || 3030;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
